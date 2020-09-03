ca2e6080cf4554f9192f07d885f90df4d356a975
Jai Krishna <49020643+Jkthehero99@users.noreply.github.com>
2020-09-03 19:37:02 +0530

    Update fun.m

--- Secant Method/fun.m
+++ Secant Method/fun.m
@@ -1,6 +1,9 @@
 function [y] = fun(x)
 %y = x^2 - 5*x - 4;
 %y = cot(x) + 20*cos(x) - 20;
-y = (200*sin(x)*(1-cos(x)))/(9.8*cos(x));
+%y = (200*sin(x)*(1-cos(x)))/(9.8*cos(x));
+%y = 20*tan(x) - 20*sin(x) - 1;
+%y= x + 5*exp(-x) - 5;
+y = x^3 -9*x^2 + 90/pi;
 end
 
